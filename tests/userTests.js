408f334539c22c66dbf7847b3ca3909160280dbd
Yoyo6K <78444206+Yoyo6K@users.noreply.github.com>
2023-01-09 23:31:57 +0100

    usertest

--- tests/userTests.js
+++ tests/userTests.js
@@ -2,6 +2,8 @@ const request = require("supertest")("http://localhost:3000/users");
 const chai = require("chai");
 const expect = require("chai").expect;
 const chaiHttp = require('chai-http');
+const { string } = require("joi");
+const { Admin } = require("mongodb");
 require("../index");
 
 chai.use(chaiHttp);
@@ -23,138 +25,93 @@ describe("GET /users/find", () => {
     });
 });
 
-// /*
-//  * LOGIN
-//  */
+/*
+ * LOGIN
+ */
 
-// describe("POST /login", () => {
-//   it("it should not login a user if email is not found", done => {
-//     chai.request(server).post("/login").send({ email: "test@email.com", password: "testpassword"}).end((err, res) => {
-//       res.should.have.status(404);
-//       done();
-//     });
-//   })
-  
-//   it("it should not logging a user if password is incorrect", done => {
+describe("POST /users/login", () => {
+  it("login with email and password and give session token",
+  async () => {
+    await request
+        .post("/login")
+        .send({
+          email: "employee2@gmail.com",
+          password: "1233456789101112",
+        })
+        .then(res => {
+          expect(res.statusCode).to.equal(200);
+          console.log(res.statusCode);
+          console.log(res.body.token)
+        })
+        .catch(error => {
+          throw new Error(error)
+        })
+    });
+  });
 
-//     user.save((err, user) => {
-//       chai
-//         .request(server)
-//         .post("/login")
-//         .send({
-//           email: "test@email.com",
-//           password: "wrongpassword"
-//         })
-//         .end((err, res) => {
-//           res.should.have.status(401);
-//           done();
-//         });
-//     });
-//   });
+/*
+ * REGISTER
+ */
 
-//   it("it should login a user if email and password are correct", done => {
-//     // Créez un utilisateur de test avant de tester la route de connexion
-//     const user = new User({
-//       email: "test@email.com",
-//       password: "testpassword"
-//     });
-//     user.save((err, user) => {
-//       chai
-//         .request(server)
-//         .post("/login")
-//         .send({
-//           email: "test@email.com",
-//           password: "testpassword"
-//         })
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.have.property("token");
-//           done();
-//         });
-//     });
-//   });
+// Testez la route de registration avec un e-mail valide et un mot de passe valide
+  it("it should register a new user if email and password is valid, and give token",
+  async () => {
+    await request
+        .post("/register")
+        .send({
+          pseudo: "testpseudo1",
+          email: "test1@email.com",
+          password: "testpassword1231",
+          role: "Admin"
+        })
+        .then(res => {
+          expect(res.statusCode).to.equal(200);
+          console.log(res.statusCode);
+          console.log(res.body.token)
+        })
+        .catch(error => {
+          throw new Error(error)
+        })
+    });
 
-//   it("it should login a user if email and password are correct", done => {
-//     user.body((err, user) => {
-//       chai
-//         .request(server)
-//         .post("/login")
-//         .send({
-//           email: "test@email.com",
-//           password: "testpassword"
-//         })
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.have.property("token");
-//           done();
-//         });
-//     });
-//   });
-// });
-
-// /*
-//  * REGISTER
-//  */
-
-// describe("POST /register", () => {
-//   // Testez la route de registration avec un e-mail déjà enregistré
-//   it("it should not register a user if email already exists", done => {
-//     User.findOne({ email: "test@test.com" }, (err, user) => {
-//       if (err) {
-//         // Gestion de l'erreur
-//       } else if (user) {
-//         // Si l'utilisateur existe déjà, testez la route de registration
-//         chai
-//           .request(server)
-//           .post("/register")
-//           .send({
-//             email: "test@test.com",
-//             password: "testpassword"
-//           })
-//           .end((err, res) => {
-//             res.should.have.status(409);
-//             done();
-//           });
-//       } else {
-//         // Si l'utilisateur n'existe pas, créez-le d'abord avant de tester la route de registration
-//         const user = new User({
-//           email: "test@test.com",
-//           password: "testpassword"
-//         });
-//         user.save((err, user) => {
-//           if (err) {
-//             // Gestion de l'erreur
-//           } else {
-//             chai
-//               .request(server)
-//               .post("/register")
-//               .send({
-//                 email: "test@test.com",
-//                 password: "testpassword"
-//               })
-//               .end((err, res) => {
-//                 res.should.have.status(409);
-//                 done();
-//               });
-//           }
-//         });
-//       }
-//     });
-//   });
-  
-//   // Testez la route de registration avec un e-mail valide et un mot de passe valide
-//   it("it should register a new user if email is valid and password is valid", done => {
-//     chai
-//       .request(server)
-//       .post("/register")
-//       .send({
-//         email: "test@test.com",
-//         password: "testpassword"
-//       })
-//       .end((err, res) => {
-//         res.should.have.status(201);
-//         res.body.should.have.property("token");
-//         done();
-//       });
-//   });
-// });
+// Testez la route de registration avec un e-mail déjà enregistré
+  // it("it should not register a user if email already exists", () => {
+  //   async () => {
+  //     await request
+  //         users.findOne({ email: req.body.email }, (err, users) => {
+  //     if (err) {
+  //       // Gestion de l'erreur
+  //     } else if (users) {
+  //       // Si l'utilisateur existe déjà, testez la route de registration
+  //       chai
+  //         .request(server)
+  //         .post("/register")
+  //         .send({
+  //           email: "test@test.com",
+  //           password: "testpassword"
+  //         })
+  //         .end((err, res) => {
+  //           res.should.have.status(409);
+  //           done();
+  //         });
+  //     } else {
+  //       user.save((err, user) => {
+  //         if (err) {
+  //           // Gestion de l'erreur
+  //         } else {
+  //           chai
+  //             .request(server)
+  //             .post("/register")
+  //             .send({
+  //               email: "test@test.com",
+  //               password: "testpassword"
+  //             })
+  //             .end((err, res) => {
+  //               res.should.have.status(409);
+  //               done();
+  //             });
+  //         }
+  //       });
+  //     }
+  //   });
+  // });
