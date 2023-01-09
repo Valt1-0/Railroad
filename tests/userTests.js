9d39c5156db6a82346972a07973c5855407f6914
Yoyo6K <78444206+Yoyo6K@users.noreply.github.com>
2023-01-09 19:21:48 +0100

    usertest

--- tests/userTests.js
+++ tests/userTests.js
@@ -25,3 +25,139 @@ describe("GET /users/find", () => {
     expect(res.status).to.eql(200);
   });
 });
+
+/*
+ * LOGIN
+ */
+
+describe("POST /login", () => {
+  it("it should not login a user if email is not found", done => {
+    chai.request(server).post("/login").send({ email: "test@email.com", password: "testpassword"}).end((err, res) => {
+      res.should.have.status(404);
+      done();
+    });
+  })
+  
+  it("it should not logging a user if password is incorrect", done => {
+
+    user.save((err, user) => {
+      chai
+        .request(server)
+        .post("/login")
+        .send({
+          email: "test@email.com",
+          password: "wrongpassword"
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+  });
+
+  it("it should login a user if email and password are correct", done => {
+    // Créez un utilisateur de test avant de tester la route de connexion
+    const user = new User({
+      email: "test@email.com",
+      password: "testpassword"
+    });
+    user.save((err, user) => {
+      chai
+        .request(server)
+        .post("/login")
+        .send({
+          email: "test@email.com",
+          password: "testpassword"
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("token");
+          done();
+        });
+    });
+  });
+
+  it("it should login a user if email and password are correct", done => {
+    user.body((err, user) => {
+      chai
+        .request(server)
+        .post("/login")
+        .send({
+          email: "test@email.com",
+          password: "testpassword"
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("token");
+          done();
+        });
+    });
+  });
+});
+
+/*
+ * REGISTER
+ */
+
+describe("POST /register", () => {
+  // Testez la route de registration avec un e-mail déjà enregistré
+  it("it should not register a user if email already exists", done => {
+    User.findOne({ email: "test@test.com" }, (err, user) => {
+      if (err) {
+        // Gestion de l'erreur
+      } else if (user) {
+        // Si l'utilisateur existe déjà, testez la route de registration
+        chai
+          .request(server)
+          .post("/register")
+          .send({
+            email: "test@test.com",
+            password: "testpassword"
+          })
+          .end((err, res) => {
+            res.should.have.status(409);
+            done();
+          });
+      } else {
+        // Si l'utilisateur n'existe pas, créez-le d'abord avant de tester la route de registration
+        const user = new User({
+          email: "test@test.com",
+          password: "testpassword"
+        });
+        user.save((err, user) => {
+          if (err) {
+            // Gestion de l'erreur
+          } else {
+            chai
+              .request(server)
+              .post("/register")
+              .send({
+                email: "test@test.com",
+                password: "testpassword"
+              })
+              .end((err, res) => {
+                res.should.have.status(409);
+                done();
+              });
+          }
+        });
+      }
+    });
+  });
+  
+  // Testez la route de registration avec un e-mail valide et un mot de passe valide
+  it("it should register a new user if email is valid and password is valid", done => {
+    chai
+      .request(server)
+      .post("/register")
+      .send({
+        email: "test@test.com",
+        password: "testpassword"
+      })
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.have.property("token");
+        done();
+      });
+  });
+});
